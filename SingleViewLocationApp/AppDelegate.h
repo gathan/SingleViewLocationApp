d8537de627e229034237e3d377e5e5ab3c631fb2
giorgath <g.athanasopoulos.it@gmail.com>
2015-04-28 23:25:42 +0300

    Initial Commit

---
+++ SingleViewLocationApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SingleViewLocationApp
+//
+//  Created by Georgios Athanasopoulos on 4/28/15.
+//  Copyright (c) 2015 GA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SingleViewLocationApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SingleViewLocationApp
+//
+//  Created by Georgios Athanasopoulos on 4/28/15.
+//  Copyright (c) 2015 GA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
